24abeda4f5e9765a0819ded35b4fd596687d3725
BRAD Technologies <bradtech.ke@gmail.com>
2019-10-29 20:19:26 +0300

    updated the comments in DarajaAPIException.

--- src/main/java/brad/tech/web/safaricom/mpesa/DarajaAPIException.java
+++ src/main/java/brad/tech/web/safaricom/mpesa/DarajaAPIException.java
@@ -26,13 +26,30 @@ package brad.tech.web.safaricom.mpesa;
  * <p>
  * This will be available for most exceptions that will likely be thrown by the REST api.
  * The only requirement is for the user to provide a message at the very least.
+ * <p>
+ * NOTE: It is NOT extensible.
  */
-public class DarajaAPIException extends Exception {
+public final class DarajaAPIException extends Exception {
 
+    /**
+     * A simple constructor with a message.
+     * <p>
+     * This satisfies the condition that a message should be supplied.
+     *
+     * @param message the message explaining the exception.
+     */
     public DarajaAPIException(String message) {
         super(message);
     }
 
+    /**
+     * A constructor that provides a message and a reference to the cause.
+     * <p>
+     * This provides wrapper functionality for any exception thrown by the APIs.
+     *
+     * @param message The message describing the exception.
+     * @param cause The root cause of the exception.
+     */
     public DarajaAPIException(String message, Throwable cause) {
         super(message, cause);
     }
