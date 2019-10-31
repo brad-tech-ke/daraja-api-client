451e950307570e93474c161996cbe1583082c6f7
BRAD Technologies <bradtech.ke@gmail.com>
2019-10-31 23:55:05 +0300

    removed the licence header from all the files.

--- src/main/java/brad/tech/web/safaricom/daraja/ResourceBundleConsumer.java
+++ src/main/java/brad/tech/web/safaricom/daraja/ResourceBundleConsumer.java
@@ -1,24 +1,3 @@
-/*
- * Copyright (c) 2019 BRAD Technologies and/or its affiliates.
- * All rights reserved. Use is subject to license terms.
- *
- * This file is available and licensed under the following license:
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, IS NOT permitted.
-
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package brad.tech.web.safaricom.daraja;
 
 import java.util.ResourceBundle;
@@ -27,6 +6,5 @@ import java.util.ResourceBundle;
  * This provides resource bundle access for the library.
  */
 public interface ResourceBundleConsumer {
-
-    ResourceBundle IL8N = ResourceBundle.getBundle("brad.tech.web.safaricom.daraja.il8n.ResourceBundle");
+    ResourceBundle il8n = ResourceBundle.getBundle("brad.tech.web.safaricom.daraja.il8n.ResourceBundle");
 }
