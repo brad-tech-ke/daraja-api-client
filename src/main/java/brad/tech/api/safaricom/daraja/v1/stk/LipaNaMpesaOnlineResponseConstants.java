edc74c2e8cce7586650cb326a2b70605daeb55a7
BRAD Technologies <bradtech.ke@gmail.com>
2020-03-28 17:41:32 +0300

    Created an interface to hold response constants

---
+++ src/main/java/brad/tech/api/safaricom/daraja/v1/stk/LipaNaMpesaOnlineResponseConstants.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2020 BRAD Technologies and/or its affiliates.
+ * All rights reserved. Use is subject to license terms.
+ *
+ * This file is available and licensed under the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, IS NOT permitted.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package brad.tech.api.safaricom.daraja.v1.stk;
+
+public interface LipaNaMpesaOnlineResponseConstants {
+
+    /**
+     * Merchant Request ID
+     */
+    String MERCHANT_REQUEST_ID = "MerchantRequestID";
+
+    /**
+     * Check out Request ID
+     */
+    String CHECKOUT_REQUEST_ID = "CheckoutRequestID";
+
+    /**
+     * Response Code
+     */
+    String RESPONSE_CODE = "ResponseCode";
+
+    /**
+     * Result Desc
+     */
+    String RESULT_DESC = "ResultDesc";
+
+    /**
+     * Response Description message
+     */
+    String RESPONSE_DESCRIPTION = "ResponseDescription";
+
+    /**
+     * Result Code
+     */
+    String RESULT_CODE = "ResultCode";
+}
