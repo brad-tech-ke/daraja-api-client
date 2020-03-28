3e54d3de3817883323ee25ff7b4107c86390c866
BRAD Technologies <bradtech.ke@gmail.com>
2020-03-28 18:11:28 +0300

    Created a test for stk push

---
+++ src/test/java/brad/tech/api/safaricom/daraja/auth/LipaNaMpesaStkPushTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020 BRAD Technologies and/or its affiliates.
+ * All rights reserved. Use is subject to license terms.
+ *
+ * This file is available and licensed under the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, IS NOT permitted.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package brad.tech.api.safaricom.daraja.auth;
+
+import brad.tech.api.safaricom.daraja.SandboxURLs;
+import brad.tech.api.safaricom.daraja.v1.stk.LipaNaMpesaSTKPushRequest;
+import org.junit.jupiter.api.Test;
+
+public class LipaNaMpesaStkPushTest {
+
+    @Test
+    public void testStkPush() {
+        final LipaNaMpesaSTKPushRequest request;
+        final String url = SandboxURLs.LIPA_NA_MPESA_STK_PUSH_URL;
+    }
+}
