3f7915cf40a41546a14494591aa0fb50d9e02191
BRAD Technologies <bradtech.ke@gmail.com>
2020-03-28 18:13:30 +0300

    Implemented Request Constants for consistent variables

---
+++ src/main/java/brad/tech/api/safaricom/daraja/v1/stk/LipaNaMPesaOnlineRequestBase.java
@@ -0,0 +1,54 @@
+package brad.tech.api.safaricom.daraja.v1.stk;
+
+import brad.tech.api.safaricom.daraja.KeyValuePair;
+
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class LipaNaMPesaOnlineRequestBase implements KeyValuePair, LipaNaMpesaOnlineRequestConstants {
+
+    protected String businessShortCode, passKey;
+    protected Date timestamp;
+
+    public String getBusinessShortCode() {
+        return businessShortCode;
+    }
+
+    public void setBusinessShortCode(String businessShortCode) {
+        this.businessShortCode = businessShortCode;
+    }
+
+    public String getPassKey() {
+        return passKey;
+    }
+
+    public void setPassKey(String passKey) {
+        this.passKey = passKey;
+    }
+
+    protected String generatePassword() {
+        final String encodeString = String.format("%s%s%s", this.businessShortCode, this.passKey, this.timestamp);
+        return Base64.getEncoder().encodeToString(encodeString.getBytes());
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public Map<String, String> getKeyValuePair() {
+        final String formattedDateTime = new SimpleDateFormat("yyyyMMddhhmmss").format(getTimestamp());
+        return new HashMap<String, String>() {{
+            put(BUSINESS_SHORT_CODE, getBusinessShortCode());
+            put(PASSWORD, generatePassword());
+            put(TIMESTAMP, formattedDateTime);
+        }};
+    }
+}
